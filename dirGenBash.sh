75c6212cfb03f42f419a0f5dbd71962dd0de8eb8
Jose Benhumea <joseramosb@gmail.com>
2023-01-03 22:41:37 -0600

    Se agrega el shebag #para que el interprete sea la zsh ya que si no intentara usar la bash y para la bash el codigo es un poco diferente.Ademas de que se debe de generar branch para cada tipo de shell

--- dirGenBash.sh
+++ dirGenBash.sh
@@ -1,11 +1,14 @@
-#Creara una carpeta que llevara como nombre el año actual y dentro de las misma creara una carpeta con los 12 meses de año presedidos por 3 digitos
-#cabe mencionar que el script funciona en unas bash
+#Se declara el shebang para especificar el interprete de comandos  que se debe utilizar para ejecutar el script
+#!/bin/zsh
+#Creara una carpeta que llevara como nombre el año actual y dentro de las misma creara una carpeta con los 12 meses de año presedidos por 3 digits
+#Cabe mencionar que el script funciona en unas bash
 #obtenemos el año actual
 ano=$(date +%Y)
 
 #creamos la carpeta
 mkdir -p "$ano"
-# creamos un array con el nombre de los meses
+# creamos un array con el nombre de los meses.
+# Por algun motivo si pongo en la misma linea todo el array, al momento de ejecutarlo arroja el error `Syntax error: "(" unexpected` when creating an array
 meses=("001Enero" 
 "002Febrero" "003Marzo" "004Abril" "005Mayo" "006Junio" "007Julio" "008Agosto" "009Septiembre" "010Octubre" "011Noviembre" "012Diciembre")
 
