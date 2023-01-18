4b7cada6b609041a7dc62c4b10b889b77b7828bc
Jose Benhumea <joseramosb@gmail.com>
2023-01-18 00:09:38 -0600

    Version 1.1. Se agrega banner de creador generado en http://www.network-science.de/ascii/ con la fuente graffiti. Ademas que se incluye una nueva funcion que nos indica el numero de archivos repetidos

--- superDiff.sh
+++ superDiff.sh
@@ -1,17 +1,27 @@
 #!/bin/zsh
 
+echo "
+ __      __        .__   __   .__ 
+/  \    /  \_____  |  | |  | _|__|
+\   \/\/   /\__  \ |  | |  |/ /  |
+ \        /  / __ \|  |_|    <|  |
+  \__/\  /  (____  /____/__|_ \__|
+       \/        \/          \/   
+
+"
+
 folder1=$1
 folder2=$2
 
 echo "Listando archivos en $folder1:"
-# Obtener lista de archivos en folder1 y mostrando detalles con find
+# Obtener lista de archivos en folder1  con find y solo mostrar archivos
 files1=($(find $folder1 -type f))
 
 echo "Listando archivos en $folder2:"
-# Obtener lista de archivos en folder2 y mostrando detalles con find
+# Obtener lista de archivos en folder2  con find y solo mostrar archivos
 files2=($(find $folder2 -type f))
 
-flag=0
+duplicate_count=0
 
 # Print header
 printf "%-40s %-40s\n" "Archivos en $folder1" "Archivos en $folder2"
@@ -24,14 +34,16 @@ do
   do
     # Comparar los nombres de archivo
     if [[ $file1 == $file2 ]]; then
-        flag=1
+        duplicate_count=$((duplicate_count+1))
         # Print file names in columns in red
         printf "\033[31m%-40s %-40s\033[0m\n" $file1 $file2
     fi
   done
 done
 
-if [ $flag -eq 0 ]
+if [ $duplicate_count -eq 0 ]
 then
     echo "No se encontraron archivos duplicados"
+else
+    echo "Se encontraron $duplicate_count archivos duplicados"
 fi
