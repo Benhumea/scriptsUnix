2fa3d36a03467a1a5a554fb60930cb96853515e6
Jose Benhumea <joseramosb@gmail.com>
2023-01-22 06:23:40 -0600

    creamos la rama para dir maker.DirMaker v3.0. Se crea para modificar ajusten en la creacion de las carpetas por semana

--- dirMaker.ps1
+++ dirMaker.ps1
@@ -1,6 +1,6 @@
 #El siguiente script tiene como finalidad crear una carpeta que se nombre como el año en curso, dentro de esa carpeta crea una carpeta por mes y dentro de cada carpeta por mes crea una por cada semana, comenzando la semana en domingo.
 
-#Version 2.0 Se agrega la funcionalidad de las carpetas por semana
+#Version 3.0 No recuerdo que version era mas funcional si la anterior o esta
 
 # Obtener el año actual
 $year = (Get-Date).Year
@@ -8,27 +8,21 @@ $year = (Get-Date).Year
 New-Item -ItemType Directory -Path ".\$year" -ErrorAction Ignore
 # Iterar a través de cada mes del año
 for ($i = 1; $i -le 12; $i++) {
-  # Obtener el nombre del mes
-  $monthName = (Get-Date -Month $i -Year $year).ToString("MMMM")
-  # Crear una carpeta con el nombre del mes
-  New-Item -ItemType Directory -Path ".\$year\$($i.ToString("000"))$monthName" -ErrorAction Ignore
-  # Obtener el primer día del mes
-  $firstDayOfMonth = (Get-Date -Month $i -Year $year).Date
-  # Obtener el último día del mes
-  $lastDayOfMonth = (Get-Date -Month $i -Year $year).AddMonths(1).AddDays(-1).Date
-  # Iterar a través de cada semana del mes
-  while ($firstDayOfMonth -le $lastDayOfMonth) {
-    # Obtener el primer día de la semana
-    $firstDayOfWeek = $firstDayOfMonth
-    # Obtener el último día de la semana
-    $lastDayOfWeek = $firstDayOfMonth.AddDays(6)
-    # Si el último día de la semana es mayor que el último día del mes, ajustar el último día de la semana al último día del mes
-    if ($lastDayOfWeek -gt $lastDayOfMonth) {
-      $lastDayOfWeek = $lastDayOfMonth
+    # Obtener el nombre del mes
+    $monthName = (Get-Date -Month $i -Year $year).ToString("MMMM")
+    # Crear una carpeta con el nombre del mes
+    New-Item -ItemType Directory -Path ".\$year\$($i.ToString("000"))$monthName" -ErrorAction Ignore
+    # Obtener el primer día del mes
+    $firstDayOfMonth = (Get-Date -Month $i -Year $year).Date
+    # Obtener el último día del mes
+    $lastDayOfMonth = (Get-Date -Month $i -Year $year).AddMonths(1).AddDays(-1).Date
+    # Iterar a través de cada semana del mes
+    while ($firstDayOfMonth -le $lastDayOfMonth) {
+        # Obtener el número de semana del mes
+        $weekOfMonth = Get-WeekOfYear -WeekOfYear $firstDayOfMonth.DayOfYear -Year $firstDayOfMonth.Year
+        # Crear una carpeta con el número de semana y el nombre del mes
+        New-Item -ItemType Directory -Path ".\$year\$($i.ToString("000"))$monthName\Semana $weekOfMonth - $monthName" -ErrorAction Ignore
+        # Actualizar el primer día del mes al siguiente domingo
+        $firstDayOfMonth = $firstDayOfMonth.AddDays(7)
     }
-    # Crear una carpeta con el rango de fechas de la semana
-    New-Item -ItemType Directory -Path ".\$year\$($i.ToString("000"))$monthName\Semana $($firstDayOfWeek.ToString("dd-MM-yyyy")) - $($lastDayOfWeek.ToString("dd-MM-yyyy"))" -ErrorAction Ignore
-    # Actualizar el primer día del mes al siguiente lunes
-    $firstDayOfMonth = $firstDayOfMonth.AddDays(7)
-  }
 }
